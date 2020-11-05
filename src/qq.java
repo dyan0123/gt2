27836d05b566668752a2e9a272a2dbf3cf275147
DYan <dyan0123@sina.com>
2020-11-05 20:14:13 +0800

    testgittt

---
+++ src/qq.java
@@ -0,0 +1,16 @@
+
+
+public class qq {
+
+    public static void main(String[] args) {
+
+        for (int aa = 0; aa < 34; aa++) {
+
+        }
+        int arr[] = {12,33,55,33};
+        if (arr == null) {
+
+        }
+
+    }
+}
